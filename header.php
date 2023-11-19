86e60957154dd85ded04678daa43df3c713f9cd3
cedricbeau <cedric7693@hotmail.fr>
2023-11-19 20:04:02 +0100

    Add files via upload

--- header.php
+++ header.php
@@ -1,24 +1,4 @@
 <header
-    x-data="{ pages: [
-        {
-            name: 'Home',
-            url: './index.php'
-        },
-        {
-            name: 'Tehilim',
-            url: './tehilim.php'
-        }
-    ]}"
     class="flex justify-center items-center h-14 py-2.5 px-4 bg-blueIsrael">
-    <ul class="flex gap-4">
-        <template x-for="(page, index) in pages" :key="index">
-            <li>
-                <a
-                    :href="page.url"
-                    :title="page.name"
-                    class="py-1 px-4 text-clampSm text-white border border-white rounded hover:bg-white hover:text-blueIsrael hover:border-white"
-                    x-text="page.name"></a>
-            </li>
-        </template>
-    </ul>
+        <h1 class="text-2xl text-white">Tanakh</h1>
 </header>
